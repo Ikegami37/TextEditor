410aa6685e95254cd7fdbc8c9afc901c139d8853
ikegami37 <referee47.gami@gmail.com>
2022-02-17 19:27:27 +0900

    メニューバーを追加しました。

--- Main.java
+++ Main.java
@@ -11,7 +11,14 @@ class MyWindow extends Frame {
         setTitle("テキストエディタ");
         setSize(800, 600);
         TextArea ta = new TextArea();
-        add(ta);
+        add(ta);MenuBar mbar = new MenuBar();
+        Menu menu = new Menu("ファイル");
+        MenuItem mopen =  new MenuItem("開く");
+        MenuItem msave =  new MenuItem("保存");
+        menu.add(mopen);
+        menu.add(msave);
+        mbar.add(menu);
+        setMenuBar(mbar);
         setVisible(true);
         addWindowListener(new WinListener() );
     }
