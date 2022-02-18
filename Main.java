361bdef71da23a8328e0129fb52bc77bb32c5e0d
Ikegami37 <92580968+Ikegami37@users.noreply.github.com>
2022-02-18 20:18:57 +0900

    メニュー項目を反応させる

--- Main.java
+++ Main.java
@@ -6,21 +6,27 @@ class Main {
     }
 }
 
-class MyWindow extends Frame {
+class MyWindow extends Frame implements ActionListener {
+    TextArea ta;
     MyWindow() {
         setTitle("テキストエディタ");
         setSize(800, 600);
-        TextArea ta = new TextArea();
+        ta = new TextArea();
         add(ta);MenuBar mbar = new MenuBar();
         Menu menu = new Menu("ファイル");
         MenuItem mopen =  new MenuItem("開く");
         MenuItem msave =  new MenuItem("保存");
+        mopen.addActionListener(this);
+        msave.addActionListener(this);
         menu.add(mopen);
         menu.add(msave);
         mbar.add(menu);
         setMenuBar(mbar);
         setVisible(true);
         addWindowListener(new WinListener() );
+    }
+    public void actionPerformed(ActionEvent e) {
+        ta.setText("メニュー項目が選ばれました")
     }
 }
 class WinListener extends WindowAdapter {
